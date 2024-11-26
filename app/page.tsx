1bfefe375856f7174a947b5c031ca0de019a58ea
Joshua Carroll <carrolljosh1976@gmail.com>
2024-11-25 20:21:46 -0500

    created dashboard page and header for application

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {
   ServerCogIcon,
   ZapIcon
 } from "lucide-react"
+import Image from "next/image";
 import Link from "next/link";
 
 const features = [
@@ -18,27 +19,27 @@ const features = [
   {
     title: "Blazing Fast Responses",
     description: "Experience lightning-fast responses to your inqueries, ensuring you get the information you need instantly.",
-    image: ZapIcon,
+    icon: ZapIcon,
   },
   {
     title: "Chat Memorisation",
     description: "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
-    image: BrainCogIcon,
+    icon: BrainCogIcon,
   },
   {
     title: "Interactive PDF Viewer",
     description: "Engage with your PDFs like never before using our intuitive and interactive viewer.",
-    image: EyeIcon,
+    icon: EyeIcon,
   },
   {
     title: "Cloud Backup",
     description: "Rest assured knowing your documents are safely backed up in the cloud, protected from loss or damage.",
-    image: ServerCogIcon,
+    icon: ServerCogIcon,
   },
   {
     title: "Responsive Across Devices",
     description: "access and chat with your PDFs seamlessly on any device, whether it's your desktop, tablet, or smartphone.",
-    image: MonitorSmartphoneIcon,
+    icon: MonitorSmartphoneIcon,
   },
 ]
 
@@ -60,9 +61,41 @@ export default function Home() {
                 enhancing productivity 10x fold effortlessly.
               </p>
             </div>
-            <Button>
-              <Link href="/dashboard"></Link>
+            <Button asChild className="mt-10">
+              <Link href="/dashboard">Get Started</Link>
             </Button>
+          </div>
+          <div className="relative overflow-hidden pt-16">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <Image
+                    alt="App Screenshot"
+                    src="https://i.imgur.com/VciRSTI.jpeg"
+                    width={2432}
+                    height={1442}
+                    className="mb-[-0%} rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+                  />
+                  <div aria-hidden="true" className="relative">
+                    <div className="absolute bottom-0 -x-inset-32 bg-gradient-to-t from-white/95 pt-[5%] " />
+                  </div>
+            </div>
+          </div>
+          <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+            <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6
+            gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2
+            lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+              {features.map((feature) => (
+                <div key={feature.title} className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <feature.icon
+                      aria-hidden="true"
+                      className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                     />
+                  </dt>
+
+                  <dd>{feature.description}</dd>
+                </div>
+              ))}
+            </dl>
           </div>
         </div>
       </main>
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher("/dashboard/(.*)");
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) await auth.protect()
-  })
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
   matcher: [
